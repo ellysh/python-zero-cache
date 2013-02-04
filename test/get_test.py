f2e07afacc15755c9f15987011aac6f67bb78ea4
Ilya Shpigor <petrsum@gmail.com>
2013-02-04 16:20:00 +0400

    Fix get_test according interface of the ClientWrap class

--- test/get_test.py
+++ test/get_test.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python
 
-import registrar_client
+import client_wrap
 
-client = registrar_client.RegistrarClient("get_test.log", "ipc:///var/run/zero-cache/0", 0)
+KEY_LONG = "key1"
+DATA_LONG = 1024
 
-key = "key1"
-data = "test data 1\0"
+KEY_DOUBLE = "key2"
+DATA_DOUBLE = 100.53
 
-client.WriteData(key, data, len(data))
+KEY_STRING = "key3"
+DATA_STRING = "test data"
 
-result = client.ReadData(key) + '\0'
-assert result == data
+def init_data(client):
+    client.WriteLong(KEY_LONG, DATA_LONG)
+    client.WriteDouble(KEY_DOUBLE, DATA_DOUBLE)
+    client.WriteString(KEY_STRING, DATA_STRING)
 
-result = client.ReadData("key2")
-assert result == ""
+def check_data(client):
+    assert DATA_LONG == client.ReadLong(KEY_LONG)
+    assert DATA_DOUBLE == client.ReadDouble(KEY_DOUBLE)
+    assert DATA_STRING == client.ReadString(KEY_STRING)
+
+def main():
+    client = client_wrap.ClientWrap("get_test.log", "ipc:///var/run/zero-cache/0", 0)
+    init_data(client)
+    check_data(client)
+
+if __name__ == "__main__":
+    main()
