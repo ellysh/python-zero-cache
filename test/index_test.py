472ae704e321dc4bcaf015cec948abe9eb8d941a
Ilya Shpigor <petrsum@gmail.com>
2013-03-04 13:27:25 +0400

    Minor fix in the `index_test`

--- test/index_test.py
+++ test/index_test.py
@@ -2,13 +2,13 @@
 
 import zero_cache
 
-def check_keys(client):
+def print_index(client):
     for index in range(10):
         print("%d = %d" % (index, client.ReadLong(index)))
 
 def main():
     client = zero_cache.Client()
-    check_keys(client)
+    print_index(client)
 
 if __name__ == "__main__":
     main()
